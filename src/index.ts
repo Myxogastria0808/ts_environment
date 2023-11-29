f1eb2e8fe4a9483fc83b03961918c08731b2c496
Myxogastria0808 <r.rstudio.c@gmail.com>
2023-11-30 02:57:04 +0900

    Hello

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { ABC } from './hello';
 const hello: string = 'Hello';
 const nyaki: number = 10;
 
-console.log(hello);
-console.log(`nyaki: ${nyaki}, ABC: ${ABC}`);
+// console.log(hello);
+// console.log(`nyaki: ${nyaki}, ABC: ${ABC}`);
